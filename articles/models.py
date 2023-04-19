75d531d9381cc3506a11aa2a2f33ebe9a0fd6263
Kim Gwang woon <misili304@naver.com>
2023-04-19 22:35:45 +0900

    Refac/ Article model

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return str(self.title)
