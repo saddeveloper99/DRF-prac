b4532b8253006511097b65a3e844798a8f43bb8c
Kim Gwang woon <misili304@naver.com>
2023-04-19 16:31:25 +0900

    Add/ articles app added to INSTALLED_APPS

--- drf_week2/settings.py
+++ drf_week2/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os, json
+import os
+import json
 from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
 
@@ -21,18 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secret.json')
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
-    
+
+
 def get_secret(setting):
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
-    
+
+
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
