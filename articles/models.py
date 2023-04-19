f5b7f20ad7b2b40293ad1463f170b47640644c7f
Kim Gwang woon <misili304@naver.com>
2023-04-19 16:26:10 +0900

    Add/ Article models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
