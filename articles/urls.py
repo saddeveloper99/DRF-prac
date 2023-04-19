da3f384fd76b8c807eb2d6573df504c23a7edb73
Kim Gwang woon <misili304@naver.com>
2023-04-20 00:54:52 +0900

    Feat/ func by method

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('<int:article_id>/', views.article_view, name='article_view'),
+    path('', views.aritcleAPI, name='index'),
+    path('<int:article_id>/', views.articleDetailAPI, name='article_view'),
 ]
